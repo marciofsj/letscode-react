65eec7f98eb1b9390d5140a672654c94f97b61fd
marciofsj <97961755+marciofsj@users.noreply.github.com>
2022-04-04 16:18:03 -0300

    atualizacao

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'antd';
 
-function App() {
+const App = () => {
+  
+  const onClickHome = () => {
+    console.log("Indo para a página inicial")
+  }
+
+  const onClickEquipe = () => {
+    console.log("Indo para a página da equipe")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    
+      <h1>Teste</h1>
+      <nav>
+        <Button type="link">Home</Button>
+        <Button type="link">Equipe</Button>
+        <Button type="link">Sobre Nós</Button>
+        <Button type="link">Contato</Button>
+      </nav>
+      <main>
+        <h2>Listagem</h2>
+        <Button>"Cadastrar item"</Button>
+      </main>
     </div>
-  );
-}
+)
+  }
 
 export default App;
