1bde26d6094c380b1956d16348e1164934ac68ef
marciofsj <97961755+marciofsj@users.noreply.github.com>
2022-04-06 16:44:36 -0300

    exercicio

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -7,6 +7,7 @@ const Main = () => (
   <Container>
     <h2>Listagem</h2>
     <Product />
+    <UserCard/>
   </Container>
 )
 
--- src/components/UserCard/index.js
+++ src/components/UserCard/index.js
@@ -1,9 +1,32 @@
-import Button from '../Button'
+import rickypark from './rickypark.json'
+import {Button, Buttonsection, Carrer, Category, Container, Infosection, Name, Photo, Skill, Skillsection, State } from './styles'
 
 const UserCard = () => (
     <div>
         <h2>User Name</h2>
-        <Button>Selecionar usuário</Button>
+        <Container>
+            <Category>{rickypark.category}</Category>
+            <Photo>
+                <img src= {rickypark.photo}/>
+            </Photo>
+            <Infosection>
+                <Name>{rickypark.name}</Name>
+                <State>{rickypark.state}</State>
+                <Carrer>{rickypark.carrer}</Carrer>
+            </Infosection>
+            <Buttonsection>
+                <Button>Message</Button>
+                <Button>Follow</Button>
+            </Buttonsection>
+            <Skillsection>
+                <Skill>Skills</Skill>
+                    {
+                        rickypark.skills
+                        .map(skills => 
+                            <Button>{skills}</Button>)
+                    }
+            </Skillsection>
+        </Container>
     </div>
 )
 
